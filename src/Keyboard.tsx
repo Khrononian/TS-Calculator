b94e61c485f3f5f779b7117129cd0d1e04c74c7d
Alvin <Alv.Barnes@gmail.com>
2025-07-23 23:17:59 -0400

    Added conditional rendering for classnames

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -7,11 +7,15 @@ type Props = {
 const Keyboard: React.FC<Props> = ({ changeValues }) => {
     const divValues = ['C', 'DEL', '.', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', `+/-`, '0', '=']
 
+    const renderClassNames = (key: number) => {
+        return key == 3 ? 'Divide' : key == 7 ? 'Multiply' : key == 11 ? 'Subtract' : key == 15 ? 'Add' : undefined
+    }
+
     return (
         <>
             <div className='board'>
                 {divValues.map((item, key) => 
-                    <div onClick={changeValues} key={key}>{item}</div>
+                    <div onClick={changeValues} className={renderClassNames(key)} key={key}>{item}</div>
                 )}
             </div>
         </>
