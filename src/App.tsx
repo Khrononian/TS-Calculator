92b80b21484130d42c25f398864801a4570b257c
Alvin <Alv.Barnes@gmail.com>
2025-07-23 23:15:49 -0400

    Added logic for calculator

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,36 @@ import Keyboard from './Keyboard'
 import './App.css'
 
 function App() {
-  const [value, setValue] = useState(0)
-  // USE A STRING TO PUSH THE DECIMALS THEN TURN THEM TO NUMBERS WHEN DOING OPERATIONS
-  const [decimal, setDecimal] = useState(false)
-  const [decimalString, setDecimalString] = useState('')
+  const [value, setValue] = useState('0')
+  const [decimal, setDecimal] = useState(false);
+  const operations = ['Add', 'Subtract', 'Multiply', 'Divide']
 
   const changeValues = (e: React.MouseEvent) => {
     const targetText = e.currentTarget.innerHTML
     const number = parseInt(targetText, 10)
 
-    console.log(e.currentTarget.innerHTML, 'Food', )
-    if (value == 0 && targetText != '+/-') setValue(Number(targetText))
-    else if (value.toString().match(/\d/g)?.length != 15 && targetText.toString().match(/\d/g)) setValue(prev => Number(prev.toString() + targetText))
+    console.log(e.currentTarget.innerHTML, e.currentTarget, 'Food', value[0], value.slice())
+    // if (value == 0 && targetText != '+/-') setValue(Number(targetText))
+    if (value == '0' && targetText != '+/-') setValue(targetText)
+    else if (value.toString().match(/\d/g)?.length != 15 && targetText.toString().match(/\d/g)) setValue(prev => prev + targetText)
     
-    if (targetText == 'C') setValue(0)
-    else if (targetText == 'DEL' && value !== 0) setValue(Number(value.toString().slice(0, -1)))
-    else if (targetText == '.') setValue(prev => Number(prev.toString() + '.1'))
-    else if (targetText == '+/-' && value != 0) setValue(prev => prev * -1)
+    if (targetText == 'C') {
+      setValue('0')
+      setDecimal(false)
+    }
+    else if (targetText == 'DEL' && value !== '0') setValue(value.slice(0, -1))
+    else if (targetText == '.' && decimal == false) {
+      setValue(value + '.')
+      setDecimal(true)
+    }
+    else if (targetText == '+/-' && value != '0' && value[0] != '-') setValue(prev => `-${prev}`)
+    else if (value[0] == '-') setValue(prev => prev.slice(1))
     
-    else console.log('FI', value, )
-    
-
+    // switch (operations) {
+    //   case 'Add':
+    //     addValues(targetText)
+    //     break;
+    // }
   }
 
   const addValues = (clickedNumber: string) => {
